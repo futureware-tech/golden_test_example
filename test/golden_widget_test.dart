58326ed4874624e613a95eee147614bf1c9dd630
Katarina Sheremet <kate.romanowskaya@gmail.com>
2024-10-07 14:16:25 +0200

    Add Golden test

---
+++ test/golden_widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_test_example/main.dart';
+
+void main() {
+  testWidgets('Golden test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile('main.png'),
+    );
+  });
+}
